366135e807fab3268f260371ae0900d981ed5724
Jeremy He <jeremy.he@safetyculture.io>
2019-06-19 10:58:51 +1000

    add match for index page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
-const levelTable = $("th:contains(Leave Accrued)").parents("table");
+function updateLevelBalance() {
+	console.log("loaded");
+	let levelTable = $("th:contains(Leave Accrued)").parents("table");
+	const annualLeaveTD = $(levelTable).find("td:contains(Annual Leave)").next();
+	const sickLeaveTD = $(levelTable).find("td:contains(sick leave)").next();
 
-const annualLeaveTD = $(levelTable).find("td:contains(Annual Leave)").next();
-const sickLeaveTD = $(levelTable).find("td:contains(sick leave)").next();
+	const annualLevelDay = (Number(annualLeaveTD.text().trim().split(" ")[0]) / 7.6).toFixed(2);
+	const sickLeaveDay = (Number(sickLeaveTD.text().trim().split(" ")[0]) / 7.6).toFixed(2);
 
-const annualLevelDay = (Number(annualLeaveTD.text().trim().split(" ")[0]) / 7.6).toFixed(2);
-const sickLeaveDay = (Number(sickLeaveTD.text().trim().split(" ")[0]) / 7.6).toFixed(2);
+	annualLeaveTD.text(`${annualLeaveTD.text()} (${annualLevelDay} days)`);
+	sickLeaveTD.text(`${sickLeaveTD.text()} (${sickLeaveDay} days)`);
+}
 
-annualLeaveTD.text(`${annualLeaveTD.text()} (${annualLevelDay} days)`);
-sickLeaveTD.text(`${sickLeaveTD.text()} (${sickLeaveDay} days)`);
+updateLevelBalance();
+
+const timer = setInterval(checkLeaveTableCreated, 1000);
+
+function checkLeaveTableCreated () {
+	if ($("th:contains(Leave Accrued)").parents("table").length > 0) {
+		clearInterval (timer);
+		updateLevelBalance();
+	}
+}
